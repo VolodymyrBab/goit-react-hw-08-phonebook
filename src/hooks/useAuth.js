6d3a2881e992c1d33fa4a00f7b39dc36988a8f50
VolodymyrBab <volbv@yahoo.com>
2024-01-03 21:33:06 +0100

    hooks

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+import {
+  selectError,
+  selectIsLoggedIn,
+  selectIsRefreshing,
+  selectUser,
+} from 'redux/auth/selectors';
+
+export const UseAuth = () => {
+  const user = useSelector(selectUser);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const error = useSelector(selectError);
+  return {
+    user,
+    isLoggedIn,
+    isRefreshing,
+    error,
+  };
+};
