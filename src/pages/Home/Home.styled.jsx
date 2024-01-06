363ad7f9c91fa61c69d0946a174b78b3089efcd4
VolodymyrBab <volbv@yahoo.com>
2024-01-06 15:50:54 +0100

    Update Home.styled.jsx

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -3,7 +3,8 @@ import styled from "@emotion/styled";
 export const MainText = styled.h1`
   font-size: 61px;
   font-weight: 700;
-  font-color: red;
+  color: red;
+  background-color: white;
 `;
 export const Text = styled.p`
   font-size: 24px;
